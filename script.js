d9d6545f966e2d972599ab1ffbaca838db7076b8
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-28 18:00:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-//your JS code here. If required.
-document.getElementById('submit').addEventListener('click', function(event) {
-    event.preventDefault(); // prevent form from submitting and refreshing the page
 
-    // get the username, password, and checkbox values
+document.getElementById('submit').addEventListener('click', function(event) {
+    event.preventDefault();
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     var rememberMe = document.getElementById('checkbox').checked;
 
     if (rememberMe) {
-        // if checkbox is checked, save username and password in local storage
         localStorage.setItem('username', username);
         localStorage.setItem('password', password);
     } else {
-        // if checkbox is not checked, remove username and password from local storage
         localStorage.removeItem('username');
         localStorage.removeItem('password');
     }
-
-    // show an alert
     alert('Logged in as ' + username);
 });
