9b1b8915ac69e918c58665a1e774db12f5d7c98f
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-30 20:34:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,29 +14,29 @@
 //     alert('Logged in as ' + username);
 // });
 
-document.getElementById('submit').addEventListener('click', function(event) {
-    event.preventDefault();
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var rememberMe = document.getElementById('checkbox').checked;
+// document.getElementById('submit').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+//     var rememberMe = document.getElementById('checkbox').checked;
 
-    if (rememberMe) {
-        localStorage.setItem('username', username);
-        localStorage.setItem('password', password);
-    } else {
-        localStorage.removeItem('username');
-        localStorage.removeItem('password');
-    }
-    alert('Logged in as ' + username);
-});
-if (localStorage.getItem('username')) {
-    var button = document.createElement('button');
-    button.id = 'existing';
-    button.innerText = 'Login as existing user';
-    document.body.appendChild(button);
+//     if (rememberMe) {
+//         localStorage.setItem('username', username);
+//         localStorage.setItem('password', password);
+//     } else {
+//         localStorage.removeItem('username');
+//         localStorage.removeItem('password');
+//     }
+//     alert('Logged in as ' + username);
+// });
+// if (localStorage.getItem('username')) {
+//     var button = document.createElement('button');
+//     button.id = 'existing';
+//     button.innerText = 'Login as existing user';
+//     document.body.appendChild(button);
 
-    button.addEventListener('click', function() {
-        var savedUsername = localStorage.getItem('username');
-        alert('Logged in as ' + savedUsername);
-    });
-}
+//     button.addEventListener('click', function() {
+//         var savedUsername = localStorage.getItem('username');
+//         alert('Logged in as ' + savedUsername);
+//     });
+// }
