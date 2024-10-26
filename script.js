fb92adc59e77f27295fb6b9bc69491acd32ce349
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-10-26 19:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -40,3 +40,33 @@
 //         alert('Logged in as ' + savedUsername);
 //     });
 // }
+
+document.getElementById('submit').addEventListener('click', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var rememberMe = document.getElementById('checkbox').checked;
+
+    if (rememberMe) {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+    } else {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+    }
+    alert('Logged in as ' + username);
+    if (localStorage.getItem('username')) {
+        var button = document.getElementById('existing');
+        if (!button) {
+            button = document.createElement('button');
+            button.id = 'existing';
+            button.innerText = 'Login as existing user';
+            document.body.appendChild(button);
+
+            button.addEventListener('click', function() {
+                var savedUsername = localStorage.getItem('username');
+                alert('Logged in as ' + savedUsername);
+            });
+        }
+    }
+});
